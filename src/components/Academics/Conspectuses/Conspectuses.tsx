f2bddc101ffc8eb2843fde12303e92f0813cb7d6
Joshua Paul <joshpauljtp@gmail.com>
2023-10-25 00:56:30 +0530

    Updating Conspectus section

--- src/components/Academics/Conspectuses/Conspectuses.tsx
+++ src/components/Academics/Conspectuses/Conspectuses.tsx
@@ -1,59 +1,34 @@
 import useEmblaCarousel from "embla-carousel-react";
-import Karalos from "@assets/images/Academics/Karalos.png";
 import "./styles.scss";
-import { useCallback, useState } from "react";
+import { useCallback } from "react";
 import Arrow from "@assets/images/Academics/CarouselArrow.svg";
 import Heading from "../../reusables/Heading";
+import { CONSPECTUS_DATA } from "./constants";
 
 function Conspectuses() {
-  const [emblaRef, emblaApi] = useEmblaCarousel();
-  const [index, setIndex] = useState<number>(0);
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true });
 
   const scrollPrev = useCallback(() => {
     if (emblaApi) {
-      setIndex((prev) => prev - 1);
       emblaApi.scrollPrev();
     }
   }, [emblaApi]);
 
   const scrollNext = useCallback(() => {
     if (emblaApi) {
-      console.log("next");
-      setIndex((prev) => prev + 1);
       emblaApi.scrollNext();
     }
   }, [emblaApi]);
 
-  const content = [
-    {
-      title: "The Karalos Conspectus",
-      desc: `The Karolos Conspectus is the oldest of the academy's lineages of study, begat by the sorcerer Azur. Scholars who follow in his footsteps pursue the mysteries of comets, leading to new kinds of comet sorceries, such as Glintstone Cometshard and Shard Spiral.`,
-      img: Karalos,
-    },
-    {
-      title: "2",
-      desc: `The Karolos Conspectus is the oldest of the academy's lineages of study, begat by the sorcerer Azur. Scholars who follow in his footsteps pursue the mysteries of comets, leading to new kinds of comet sorceries, such as Glintstone Cometshard and Shard Spiral.`,
-      img: Karalos,
-    },
-    {
-      title: "3",
-      desc: `The Karolos Conspectus is the oldest of the academy's lineages of study, begat by the sorcerer Azur. Scholars who follow in his footsteps pursue the mysteries of comets, leading to new kinds of comet sorceries, such as Glintstone Cometshard and Shard Spiral.`,
-      img: Karalos,
-    },
-  ];
-
   return (
     <section id="conspectuses">
-      <button className="prev" onClick={scrollPrev} disabled={index === 0}>
+      <button className="prev" onClick={scrollPrev}>
         <img src={Arrow} alt="Prev" />
       </button>
       <div className="emblaViewport" ref={emblaRef}>
         <div className="emblaContainer">
-          {content?.map(({ title, desc, img }, i: number) => (
-            <div
-              className={`slide ${i === index ? "active" : ""}`}
-              key={`conspectus-${i}`}
-            >
+          {CONSPECTUS_DATA?.map(({ title, desc, img }, i: number) => (
+            <div className={`slide`} key={`conspectus-${i}`}>
               <Heading level={2} text={title} />
               <p>{desc}</p>
               <img src={img} alt="" />
@@ -61,11 +36,7 @@ function Conspectuses() {
           ))}
         </div>
       </div>
-      <button
-        className="next"
-        onClick={scrollNext}
-        disabled={index === content?.length - 1}
-      >
+      <button className="next" onClick={scrollNext}>
         <img src={Arrow} alt="Next" />
       </button>
     </section>
---
+++ src/components/Academics/Conspectuses/constants.ts
@@ -0,0 +1,27 @@
+import Karalos from "@assets/images/Academics/Karalos.png";
+import Lazuli from "@assets/images/Academics/Lazuli.png";
+import Olivinus from "@assets/images/Academics/Olivinus.png";
+import Haima from "@assets/images/Academics/Haima.png";
+
+export const CONSPECTUS_DATA = [
+  {
+    title: "The Karalos Conspectus",
+    desc: `The Karolos Conspectus is the oldest of the academy's lineages of study, begat by the sorcerer Azur. Scholars who follow in his footsteps pursue the mysteries of comets, leading to new kinds of comet sorceries, such as Glintstone Cometshard and Shard Spiral.`,
+    img: Karalos,
+  },
+  {
+    title: "The Olivinus Conspectus",
+    desc: `The lineage of the Olivinus Conspectus began with the sorcerer Lusat, and its adherents continue his study of meteors. Those who follow this conspectus wield sorceries that launch magic shooting stars at their targets.`,
+    img: Olivinus,
+  },
+  {
+    title: "The Lazuli Conspectus",
+    desc: `A heterodox conspectus that views the moon as equal to the stars. Scholars of the Lazuli Conspectus study Carian sorceries and fight using wooden Lazuli Glintstone Swords inlaid with glintstone. They don pale Lazuli Robes in the hue of the full moon.`,
+    img: Lazuli,
+  },
+  {
+    title: "The Haima Conspectus",
+    desc: `The conspectus of Haima, the adjudicator of the Academy. Scholars of the Haima Conspectus, known as battlemages, seek to quell conflict through the use of their sorceries of cannon fire and gavel.`,
+    img: Haima,
+  },
+];
